6c9d6fee1d5ae9dd935c1359f3f7a5037382e95c
tnem <tanmay.e.mahale@gmail.com>
2025-06-04 16:43:30 +0530

    cors-updated

--- app.ts
+++ app.ts
@@ -11,7 +11,14 @@ import globalErrorHandler from "./utils/globalErrorHandler";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://synera-app-frontend.vercel.app/login",
+    ],
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
